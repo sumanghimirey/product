55b46c450a26fc5ebdc0865cff055fef4b055ab4
sumanghimirey <working.forum7@gmail.com>
2016-08-05 18:00:09 +0545

    Creating sign in and signup view

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+    <title>
+            @yield('title')
+    </title>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script
+            src="https://code.jquery.com/jquery-3.1.0.min.js"
+            integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+            crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" type="text/css">
+    {{--<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" type="text/css">--}}
+
+    {{--<script type="text/javascript" src="{{asset('assets/js/jquery-1.8.2.min.js')}}"></script>--}}
+    {{--<script type="text/javascript" src="{{asset('assets/js/bootstrap.min.js')}}"></script>--}}
+</head>
+
+<div class="container">
+
+
+        @yield('section')
+
+
+
+
+
+</div>
+
+
+@yield('script')
+</html>
